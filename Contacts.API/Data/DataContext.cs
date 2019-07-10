b5fd967a01cfcd49cef61fe61e8d2e1efb2a4f86
AP-93 <AP-93@users.noreply.github.com>
2019-07-10 19:59:37 +0200

    add Db migration

---
+++ Contacts.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Contacts.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Contact> Contacts { get; set; }
+
+        
+    }
+}
---
+++ Contacts.API/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.API.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNum { get; set; }
+    }
+}
